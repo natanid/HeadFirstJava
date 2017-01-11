e6fe8e310e03ed013d83b4723185c36e37312c58
tlevchuk <tlevchuk@blackberry.com>
2017-01-11 16:21:59 +0200

    second commit 01.11.2017

---
+++ AExercise1b.java
@@ -0,0 +1,14 @@
+package chap01;
+
+public class AExercise1b {
+
+	public static void main(String[] args) {
+		int x = 1;
+		while (x < 10) {
+			if (x > 3) {
+				System.out.println("The big X");
+			}
+		}
+	}
+
+}
